311b639c978d8c1e8e4bf9a1cd2d15d3bf39e8e1
AhmedGad <eng.ahmedmgad@gmail.com>
2021-06-29 13:59:44 +0200

    update side menu

--- config/demo1/menu.php
+++ config/demo1/menu.php
@@ -7,7 +7,7 @@ return array(
     'main'       => array(
         //// Dashboard
         array(
-            'title' => 'Dashboard',
+            'title' => 'الرئيسية',
             'path'  => 'index',
             'icon'  => Theme::getSvgIcon("icons/duotone/Design/PenAndRuller.svg", "svg-icon-2"),
         ),
@@ -15,7 +15,7 @@ return array(
         //// Modules
         array(
             'classes' => array('content' => 'pt-8 pb-2'),
-            'content' => '<span class="menu-section text-muted text-uppercase fs-8 ls-1">Modules</span>',
+            'content' => '<span class="menu-section text-muted text-uppercase fs-8 ls-1">الأقسام</span>',
         ),
 
         // Account
@@ -96,68 +96,6 @@ return array(
             'path'  => '/vaccines',
             'icon'  => Theme::getSvgIcon("icons/duotone/syringe.svg", "svg-icon-2"),
         ),
-        // System
-        array(
-            'title'      => 'System',
-            'icon'       => array(
-                'svg'  => Theme::getSvgIcon("icons/duotone/Layout/Layout-4-blocks.svg", "svg-icon-2"),
-                'font' => '<i class="bi bi-layers fs-3"></i>',
-            ),
-            'classes'    => array('item' => 'menu-accordion'),
-            'attributes' => array(
-                "data-kt-menu-trigger" => "click",
-            ),
-            'sub'        => array(
-                'class' => 'menu-sub-accordion menu-active-bg',
-                'items' => array(
-                    array(
-                        'title'      => 'Settings',
-                        'path'       => '#',
-                        'bullet'     => '<span class="bullet bullet-dot"></span>',
-                        'attributes' => array(
-                            'link' => array(
-                                "title"             => "Coming soon",
-                                "data-bs-toggle"    => "tooltip",
-                                "data-bs-trigger"   => "hover",
-                                "data-bs-dismiss"   => "click",
-                                "data-bs-placement" => "right",
-                            ),
-                        ),
-                    ),
-                    array(
-                        'title'      => 'Audit Log',
-                        'path'       => '#',
-                        'bullet'     => '<span class="bullet bullet-dot"></span>',
-                        'attributes' => array(
-                            'link' => array(
-                                "title"             => "Coming soon",
-                                "data-bs-toggle"    => "tooltip",
-                                "data-bs-trigger"   => "hover",
-                                "data-bs-dismiss"   => "click",
-                                "data-bs-placement" => "right",
-                            ),
-                        ),
-                    ),
-                    array(
-                        'title'  => 'System Log',
-                        'path'   => 'log/system',
-                        'bullet' => '<span class="bullet bullet-dot"></span>',
-                    ),
-                ),
-            ),
-        ),
-
-        // Separator
-        array(
-            'content' => '<div class="separator mx-1 my-4"></div>',
-        ),
-
-        // Changelog
-        array(
-            'title' => 'Changelog v'.theme()->getVersion(),
-            'icon'  => Theme::getSvgIcon("icons/duotone/Files/File.svg", "svg-icon-2"),
-            'path'  => 'documentation/getting-started/changelog',
-        ),
     ),
 
     // Horizontal menu
@@ -166,6 +104,16 @@ return array(
         array(
             'title'   => 'الرئيسية',
             'path'    => 'index',
+            'classes' => array('item' => 'me-lg-1'),
+        ),
+        array(
+            'title'   => 'تسجيل عميل جديد',
+            'path'    => 'accounts/users/create',
+            'classes' => array('item' => 'me-lg-1'),
+        ),
+        array(
+            'title'   => 'تسجيل كلب جديد',
+            'path'    => 'dogs/create',
             'classes' => array('item' => 'me-lg-1'),
         ),
     ),
