f930e25e2bc1b935466afd3c30ae99e6e21b4360
Ebrahim Mansour <hema@gmail.com>
2021-06-30 16:23:48 +0200

    profiles-

--- resources/views/pages/dogs/profile.blade.php
+++ resources/views/pages/dogs/profile.blade.php
@@ -1,7 +1,7 @@
 <x-base-layout>
 
     <div class="card mb-3">
-        <img src="{{asset($dog->avatar)}}" height="350px" class="card-img-top" alt="...">
+        <img src="{{asset($dog->avatar)}}" height="350px" class="card-img-top" alt="{{$dog->name}} Image">
         <div class="card-body">
             <h5 class="card-title">{{$dog->name}}</h5>
             <p class="card-text">{{$dog->notes}}</p>
--- resources/views/pages/users/profile.blade.php
+++ resources/views/pages/users/profile.blade.php
@@ -1,7 +1,7 @@
 <x-base-layout>
 
     <div class="card mb-3">
-        <img src="url({{$user->avatar}})" height="350px" class="card-img-top" alt="...">
+        <img src="url({{$user->avatar}})" height="350px" class="card-img-top" alt="{{$user->name}} Profile Image">
         <div class="card-body">
             <h5 class="card-title">{{$user->name}}</h5>
             <p class="card-text">{{$user->email}}</p>
