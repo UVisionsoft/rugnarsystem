9838748ffa2c5d93cb881b372e016f5b4ea6563a
Ebrahim Mansour <hema@gmail.com>
2021-06-30 14:09:37 +0200

    edites in user dash page

--- resources/views/pages/dashboard/user.blade.php
+++ resources/views/pages/dashboard/user.blade.php
@@ -5,17 +5,17 @@
             <h3 class="card-title">كلابي</h3>
         </div>
         <div class="card-body">
-    <table class="table table-sm">
-       <tr>
+    <table class="table align-middle table-row-dashed fs-6 gy-5 dataTable no-footer">
+       <thead>
            <th>#</th>
            <th>اسم الكلب</th>
            <th>خيارات</th>
-       </tr>
+       </thead>
         @foreach($dogs as $key => $dog)
         <tr>
             <td>{{++$key}}</td>
             <td>{{$dog->name}}</td>
-            <td><a class="btn btn-success btn-sm">Show</a></td>
+            <td><a class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a></td>
         </tr>
         @endforeach
     </table>
