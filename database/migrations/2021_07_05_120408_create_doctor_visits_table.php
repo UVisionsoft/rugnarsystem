9f64fc00f6a41c9908fd959272f1eec6fbad7e02
Ebrahim Mansour <hema@gmail.com>
2021-07-05 14:44:10 +0200

    doctor visites migrations

---
+++ database/migrations/2021_07_05_120408_create_doctor_visits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor_visits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class)->restrictOnDelete();
+            $table->foreignIdFor(\App\Models\Dog::class)->cascadeOnDelete();
+            $table->timestamp('visited_at');
+            $table->text('notes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor_visits');
+    }
+}
