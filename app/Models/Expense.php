8b9a6355870ceed559950694a11b68a6b1c2fb35
AhmedGad <eng.ahmedmgad@gmail.com>
2021-07-11 12:59:54 +0200

    add traits to model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\MoneyTrait;
+use App\Traits\TransactableTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    use HasFactory;
+    use HasFactory, MoneyTrait, TransactableTrait;
 
     protected $fillable = ['name','amount','notes'];
 
